ba3aeeed4b344a16cbbcfcce044a2a13296860c3
timotius-jason <timotiusjason@u.nus.edu>
2022-10-12 14:22:28 +0700

    Add gstreamer support to the FrameLoader

--- src/main/services/FrameLoaderService/FrameLoader.py
+++ src/main/services/FrameLoaderService/FrameLoader.py
@@ -1,9 +1,11 @@
 from typing_extensions import Self
-import cv2 
+import cv2
+
+GSTREAMER_SOURCE = "nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM), width=1920, height=1080, framerate=30/1 ! nvvidconv flip-method=2 ! video/x-raw, width=960, height=540, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink"
 
 class FrameLoaderOpenCV:
-    def __init__(self, source, width: int = None, height: int = None) -> None:
-        self.video = cv2.VideoCapture(source)
+    def __init__(self, source = GSTREAMER_SOURCE, width: int = None, height: int = None) -> None:
+        self.video = cv2.VideoCapture(source, cv2.CAP_GSTREAMER)
         if width is not None and width <= self.video.get(cv2.CAP_PROP_FRAME_WIDTH):
             self.video.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 
@@ -21,3 +23,4 @@ class FrameLoaderOpenCV:
         cv2.destroyAllWindows()
         print("Exited FrameLoaderOpenCV")
         return True
+
